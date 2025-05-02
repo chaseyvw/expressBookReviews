c9e28588fdfab0a4898dbbd7c8a71dc0ed950832
chaseylorraine <chaseyvw@gmail.com>
2025-05-02 14:43:47 -0700

    initial commit

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -6,7 +6,7 @@ const regd_users = express.Router();
 let users = [];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+//write code to check is the username is valid 
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
@@ -15,7 +15,7 @@ const authenticatedUser = (username,password)=>{ //returns boolean
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
+  //Write your code here testing
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
